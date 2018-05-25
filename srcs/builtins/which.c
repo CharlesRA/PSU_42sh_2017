33f7f2490a9461b864e7c8dc467f9bd08369448a
Charles Raimbault <charles.raimbault@epitech.eu>
2018-05-25 17:42:01 +0200

    Major update : which builtin

--- srcs/builtins/which.c
+++ srcs/builtins/which.c
@@ -10,14 +10,16 @@
 
 char **display_which(shell_t *tcsh, char **env)
 {
-	if (my_array_len(tcsh->different_command[0]) == 1)
+	if (my_array_len(tcsh->different_command[0]) == 1) {
+		fprintf(stderr, "which: Too few arguments.\n");
 		tcsh->return_value = 1;
+	}
 	for (int i = 1; tcsh->different_command[0][i] != NULL; i++) {
 		if (check_builtin(tcsh->different_command[0][i]) == 1) {
 			printf("%s: shell built-in command.\n"
 			, tcsh->different_command[0][i]);
-			fflush(stdout);
 		}
 
 	}
+	fflush(stdout);
 }
